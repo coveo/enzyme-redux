0364b2c9086a410b806f0f79fdbd543e06d83ee5
dmgauthier <dgauthier@coveo.com>
2020-12-04 12:41:43 -0500

    feat(utils): adapt to react-redux 7

--- src/index.js
+++ src/index.js
@@ -1,37 +1,59 @@
-import {shallow, mount} from 'enzyme';
+import {mount, shallow} from 'enzyme';
+import React from 'react';
+import {Provider} from 'react-redux';
 
 export const shallowWithStore = (Component, store) => {
-    const context = {
-        store,
-    };
-    return shallow(Component, {context});
+    const ReactReduxComponent = React.cloneElement(Component, {store}, null);
+
+    return shallow(ReactReduxComponent);
+};
+
+export const shallowWithStoreAndDive = (Component, store) => {
+    const ReactReduxComponent = React.cloneElement(Component, {store}, null);
+
+    return shallow(ReactReduxComponent).dive();
 };
 
 export const mountWithStore = (Component, store) => {
-    const context = {
-        store,
-    };
-    return mount(Component, {context});
+    const ComponentAlreadyWithProvider = React.cloneElement(Component, {store}, null);
+    const ReactReduxComponent = <Provider store={store}>{Component}</Provider>;
+
+    return mount(withContext ? ComponentAlreadyWithProvider : ReactReduxComponent);
 };
 
 export const shallowWithState = (Component, state) => {
-    const context = {
-        store: {
-            getState: () => state,
-            subscribe: () => ({}),
-            dispatch: () => ({}),
-        },
+    const store = {
+        getState: () => state,
+        subscribe: () => ({}),
+        dispatch: () => ({}),
     };
-    return shallow(Component, {context});
+
+    const ReactReduxComponent = React.cloneElement(Component, {store}, null);
+
+    return shallow(ReactReduxComponent);
+};
+
+export const shallowWithStateAndDive = (Component, state) => {
+    const store = {
+        getState: () => state,
+        subscribe: () => ({}),
+        dispatch: () => ({}),
+    };
+
+    const ReactReduxComponent = React.cloneElement(Component, {store}, null);
+
+    return shallow(ReactReduxComponent).dive();
 };
 
 export const mountWithState = (Component, state) => {
-    const context = {
-        store: {
-            getState: () => state,
-            subscribe: () => ({}),
-            dispatch: () => ({}),
-        },
+    const store = {
+        getState: () => state,
+        subscribe: () => ({}),
+        dispatch: () => ({}),
     };
-    return mount(Component, {context});
+
+    const ComponentAlreadyWithProvider = React.cloneElement(Component, {store}, null);
+    const ReactReduxComponent = <Provider store={store}>{Component}</Provider>;
+
+    return mount(withContext ? ComponentAlreadyWithProvider : ReactReduxComponent);
 };
